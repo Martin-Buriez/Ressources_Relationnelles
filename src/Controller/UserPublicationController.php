77b4f937b3453f3057aa810910c8990eda2fab4d
Cyprien <cypsio23@gmail.com>
2023-02-17 23:45:22 +0100

    Page Publication by utilisateur

--- src/Controller/UserPublicationController.php
+++ src/Controller/UserPublicationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Publication;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserPublicationController extends AbstractController
 {
     #[Route('/mon-profil/mes-publications', name: 'user_publication')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $publications =  $entityManager->getRepository(Publication::class)->findAll();
+
         return $this->render('user_publication/index.html.twig', [
             'controller_name' => 'UserPublicationController',
         ]);
