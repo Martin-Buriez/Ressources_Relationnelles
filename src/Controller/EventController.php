1084b9e5cb5d19c319d707257c8c8ec970738b07
Cyprien <cypsio23@gmail.com>
2023-03-02 18:38:00 +0100

    Ajout Participant sur un event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Entity\User;
+use App\Entity\UserManageEvent;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,10 +20,48 @@ class EventController extends AbstractController
             ->where('e.stateValidated = true')
             ->getQuery()
             ->getResult();
+        
+        $user = $this->getUser();
+        $userProfile = $entityManager->getRepository(User::class)->findOneById($user);
+
+        $userManageEvents = $entityManager->getRepository(UserManageEvent::class)
+            ->createQueryBuilder('m')
+            ->where('m.User = :userProfile AND (m.IsParticipant = true OR m.IsOrganizer = true)') 
+            ->setParameter('userProfile', $userProfile)
+            ->getQuery()
+            ->getResult();
 
         return $this->render('event/index.html.twig', [
             'controller_name' => 'EventController',
-            'events' => $events
+            'events' => $events,
+            'userManageEvents' => $userManageEvents
         ]);
+    }
+
+    #[Route('/evenements/ajout/{idEvent}', name: 'user_participe_event')]
+    public function addParticipantInEvent(EntityManagerInterface $entityManager, $idEvent): Response
+    {        
+        $user = $this->getUser();
+        $userProfile = $entityManager->getRepository(User::class)->findOneById($user);
+
+        $event = $entityManager->getRepository(Event::class)->findOneById($idEvent);
+
+        $userManageEventRepository = $entityManager->getRepository(UserManageEvent::class);
+        $userManageEvent = $userManageEventRepository->findOneBy(['User' => $userProfile, 'event' => $event]);
+        
+        if ($userManageEvent !== null) {
+            // The user is managing the event
+            $this->addFlash('error', 'Vous êtes déja inscript à cet événement');
+        } else {
+            // The user is not managing the event
+            $eventManageEvent = new UserManageEvent();
+            $eventManageEvent->setUser($userProfile);
+            $eventManageEvent->setEvent($event);
+            $eventManageEvent->setIsOrganizer(false);
+            $eventManageEvent->setIsParticipant(true);
+            $entityManager->persist($eventManageEvent);
+            $this->addFlash('succes', 'Bravo Vous êtes inscript à cet événement');
+        }
+        return $this->redirectToRoute('evenements');
     }
 }
