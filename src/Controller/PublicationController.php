ca95a1a9a61919dfefef3b00b8f56558d555e277
Cyprien <cypsio23@gmail.com>
2023-03-06 18:30:19 +0100

    User create comment on publication

--- src/Controller/PublicationController.php
+++ src/Controller/PublicationController.php
@@ -2,13 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\CommentConcernPublication;
 use App\Entity\Image;
 use App\Entity\Publication;
 use App\Entity\PublicationIncludeImage;
 use App\Entity\User;
+use App\Entity\UserEditComment;
+use App\Form\CommentType;
 use App\Services\PaginationServices;
+use DateTimeZone;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,7 +29,9 @@ class PublicationController extends AbstractController
             ->setLimit(3)
             ->setEntityClass(Publication::class)
             ->setCriteria([]);
+
         $paginatePublications = $pagination->getData();
+        
         return $this->render('publication/index.html.twig', [
             'publications' => $paginatePublications,
             'pages' => $pagination->getPages(),
@@ -37,11 +45,47 @@ class PublicationController extends AbstractController
      * @return Response
      * @throws Exception
      */
-    #[Route('/les-publications/{slug}/{page<\d+>?1}', name: 'publication_show', requirements: ["page" => "\d+"])]
-    public function show(EntityManagerInterface $entityManager, $slug, PaginationServices $pagination, $page)
+    #[Route('/les-publications/{slug}/{page<\d+>?1}/{idPublication}', name: 'publication_show', requirements: ["page" => "\d+"])]
+    public function show(EntityManagerInterface $entityManager, Request $request, $slug, PaginationServices $pagination, $page, $idPublication)
     {
+        $userConnected = $this->getUser();
+        $userProfile = $entityManager->getRepository(User::class)->findOneById($userConnected);
+
         $publication = $entityManager->getRepository(Publication::class)->findOneBySlug($slug);
-        
+        $publicationSelected = $entityManager->getRepository(Publication::class)->findOneById($idPublication);
+
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
+
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            // Liaison commentaire a la publication
+            $commentConcernPublication = new CommentConcernPublication();
+            $commentConcernPublication->setComment($comment);
+            $commentConcernPublication->setPublication($publicationSelected);
+            $entityManager->persist($commentConcernPublication); 
+
+            // Liaison commentaire au créateur
+            $userEditComment = new UserEditComment();
+            $userEditComment->setComment($comment);
+            $userEditComment->setUser($userProfile);
+            $entityManager->persist($userEditComment); 
+
+            //Set date de création
+            $dateTimeZone = new DateTimeZone('Europe/Paris');
+            $date = new \DateTimeImmutable('now', $dateTimeZone);
+            $comment->setCreatedAt($date);
+
+            $comment->setReportedStatus(false);
+            
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            $this->addFlash(
+                'comment_create_succes',
+                'Votre commentaire a bien été ajouté !'
+            );
+        }
+
         $pagination
             ->setOrderBy(['created_at' => 'DESC'])
             ->setPage($page)
@@ -54,6 +98,7 @@ class PublicationController extends AbstractController
             'pages' => $pagination->getPages(),
             'page' => $page,
             'pagination' => $pagination,
+            'commentForm' => $commentForm,
         ]);
     }
 }
---
+++ src/Form/CommentType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TypeTextType::class, [
+                'label' => 'Titre du commentaire :',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Titre de votre évenement',
+                    'class' => 'form-input block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu du commentaire :',
+                'label_attr'=> [
+                    'class' => 'font-semibold'
+                ],
+                'attr' => [
+                    'rows' => '15',
+                    'placeholder' => 'Saisir la description de l\'évenement ',
+                    'class' => 'form-input block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue1 rounded-md hover:text-main-blue hover:bg-main-light-blue focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
