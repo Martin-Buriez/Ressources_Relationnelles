d1fd9fc7e970fa759c6e77d8114f3c8dd8b607f0
Maxence VANDEKERCKHOVE <maxence.vandekerckhove@viacesi.fr>
2023-03-07 13:01:21 +0100

    Small Problem Fix

--- src/Form/IdentityCardType.php
+++ src/Form/IdentityCardType.php
@@ -20,13 +20,13 @@ class IdentityCardType extends AbstractType
                 'mapped' => false,
                 'required' => true,
                 'attr' => [
-                    'class' => 'border px-4 w-1/3 bg-gray-50'
+                    'class' => 'px-4 bg-gray-50'
                 ],
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer',
                 'attr' => [
-                    'class' => 'my-8 w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue1 rounded-md hover:text-main-blue hover:bg-main-light-blue focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50'
+                    'class' => 'w-full px-4 text-white transition-colors duration-200 bg-blue1 rounded-md'
                 ]
             ])
         ;
