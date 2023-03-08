36cfbc85552bdc579a4d853d9758413e16ef0daa
Maxence VANDEKERCKHOVE <maxence.vandekerckhove@viacesi.fr>
2023-03-09 00:56:52 +0100

    Chat Front Remake

--- src/Form/MessageToUserType.php
+++ src/Form/MessageToUserType.php
@@ -16,20 +16,20 @@ class MessageToUserType extends AbstractType
     {
         $builder
             ->add('content', TextType::class, [
-                'label' => 'Saisie de votre message :',
+                'label' => false,
                 'label_attr'=> [
                     'class' => 'font-semibold'
                 ],
                 'attr' => [
                     'rows' => '15',
                     'placeholder' => 'Saisir votre message',
-                    'class' => 'form-input block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+                    'class' => 'form-input block w-full px-4 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-800 rounded-2xl dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
                 ]
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer',
                 'attr' => [
-                    'class' => 'my-8 w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue1 rounded-md hover:text-main-blue hover:bg-main-light-blue focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50'
+                    'class' => 'w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue1 rounded-md hover:text-main-blue hover:bg-main-light-blue focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50'
                 ]
             ])
         ;
